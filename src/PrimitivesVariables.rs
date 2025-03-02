fcedaaae363e4f043200b784e99dc038560634e0
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-03-03 01:51:15 +0300

    Ultimate Sophistication

--- src/PrimitivesVariables.rs
+++ src/PrimitivesVariables.rs
@@ -1,26 +1,33 @@
-/*
-fn main() {
-    println!("Lets start the journey we all");
-}
-*/
 
-/*
+fn main() {
+//How we print in Rust
+    println!("Lets start the journey we all");
+//Calling function in main
+    ints();
+    bool();
+    char();
+}
+
+
+
 //Working with Int values
-fn main(){
+fn ints(){
     let x: i32 = -10;
     let y: u64 = -90;
     println!("Signed Integer: {}", x);
     println!("Unusigned Integer: {}", y);
-} */
-/* 
-fn main(){
+} 
+
+
+//Working with Boolean Values
+fn bool(){
     let is_raining: bool = true;
     println!("It is {} raining",is_raining);
-} */
+} 
 
 
-//Working with char
-fn main(){
+//Working with char Values
+fn char(){
     let letter: char = 'a';
     println!("So the first letter of the alphabet is {}", letter);  // prints: So the first letter of the alphabet is a
 }
--- src/tuples.rs
+++
@@ -1,8 +0,0 @@
-fn main(){
-    //tuples
-    let human:(String,i32,bool) = ("Alice".to_string(),30,false);
-    println!("{:?} is {:?} years old and is {:?}",human.0, human.1, human);
-
-    let human2= ("Alice",30,false);
-    println!("{:?} is {:?} years old and is {:?}",human2.0, human2.1, human2);
-}
