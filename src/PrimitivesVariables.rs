e7e5dd2f1b63b2fa8462e17c8bc0b863f8869628
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-03-03 01:39:44 +0300

    Learning Rust a level at a time

---
+++ src/PrimitivesVariables.rs
@@ -0,0 +1,26 @@
+/*
+fn main() {
+    println!("Lets start the journey we all");
+}
+*/
+
+/*
+//Working with Int values
+fn main(){
+    let x: i32 = -10;
+    let y: u64 = -90;
+    println!("Signed Integer: {}", x);
+    println!("Unusigned Integer: {}", y);
+} */
+/* 
+fn main(){
+    let is_raining: bool = true;
+    println!("It is {} raining",is_raining);
+} */
+
+
+//Working with char
+fn main(){
+    let letter: char = 'a';
+    println!("So the first letter of the alphabet is {}", letter);  // prints: So the first letter of the alphabet is a
+}
