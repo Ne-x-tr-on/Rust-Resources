7ef89f389afc4d6329019c34c95417dfc2ed5f15
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-08-15 20:50:02 +0300

    OnTraits

--- General/Enum/src/main.rs
+++ General/Enum/src/main.rs
@@ -1,30 +1,30 @@
-// #![warn(dead_code)]
-// fn main(){
-//     enum IpAddrKind{
-//         V4,
-//         V6,
-//     }
+#![warn(dead_code)]
+fn main(){
+    enum IpAddrKind{
+        V4,
+        V6,
+    }
 
-//     // let four = IpAddrKind::V4;
-//     // let six = IpAddrKind::V6;
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
 
-//     // fn route(ip_kind:IpAddrKind){}
+    // fn route(ip_kind:IpAddrKind){}
 
-//     // route(IpAddrKind::V4)
+    // route(IpAddrKind::V4)
 
 
-//     struct IpAddr{
-//         kind:IpAddrKind,
-//         address:String,
-//     }
+    struct IpAddr{
+        kind:IpAddrKind,
+        address:String,
+    }
 
-//     let _home = IpAddr{
-//         kind:IpAddrKind::V4,
-//         address:String::from("127.0.0.1")
-//     };
+    let _home = IpAddr{
+        kind:IpAddrKind::V4,
+        address:String::from("127.0.0.1")
+    };
 
     
-// }
+}
 
 
 // fn main(){
--- General/Genandtraits/src/main.rs
+++ General/Genandtraits/src/main.rs
@@ -1,39 +1,13 @@
-#[allow(dead_code)]
-
-
-struct character {
-    hit_points: u16,
+enum Newton{
+    name,
+    gender,
+    vision,
+    title,
 }
 
 
-struct Person <PetType:Animal>{
-    first_name:String,
-    pet:PetType,
-}
-
-trait Animal{}
-
-struct Cat {}
-struct Dog{}
-
-impl Dog{
-    fn bark(&self){
-        println!("Bark");
-    }
-}
-
-impl Animal for Dog {}
-
-
-
-pub fn create_person(){
-    let pet1 = Dog{};
-    let p1 = Person{
-        first_name:"Newton".to_string(),
-        pet:pet1,
-    };
-}
-
+#[Debug]
 fn main(){
-    create_person();
+    let _name = Newton::name;
+    println!("Hey {:?}",_name);
 }
