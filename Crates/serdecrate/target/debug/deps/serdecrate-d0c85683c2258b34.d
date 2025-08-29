372ac79eea0462de31fb417129b37c2a9eb482a6
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-08-29 07:49:32 +0300

    Updates On Serde

---
+++ Crates/serdecrate/target/debug/deps/serdecrate-d0c85683c2258b34.d
@@ -0,0 +1,6 @@
+D:\PlayGround\RustPlayground\Crates\serdecrate\target\debug\deps\serdecrate-d0c85683c2258b34.d: src\main.rs src\serdecrate.rs
+
+D:\PlayGround\RustPlayground\Crates\serdecrate\target\debug\deps\libserdecrate-d0c85683c2258b34.rmeta: src\main.rs src\serdecrate.rs
+
+src\main.rs:
+src\serdecrate.rs:
