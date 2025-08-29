6f6b22a4c30303c0c92e2c6fab801065c0fc0ba1
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-08-30 01:24:55 +0300

    SprintWay

---
+++ Sprint/Ownership/target/debug/deps/Ownership-8e8bc21a08fe176c.d
@@ -0,0 +1,7 @@
+D:\playground\RustPlayground\sprint\Ownership\target\debug\deps\Ownership-8e8bc21a08fe176c.d: src\main.rs src\ownership.rs src\own_function.rs
+
+D:\playground\RustPlayground\sprint\Ownership\target\debug\deps\libOwnership-8e8bc21a08fe176c.rmeta: src\main.rs src\ownership.rs src\own_function.rs
+
+src\main.rs:
+src\ownership.rs:
+src\own_function.rs:
