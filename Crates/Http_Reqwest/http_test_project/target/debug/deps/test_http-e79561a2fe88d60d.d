47d73337d004bc7fe06054d4a0e1cd70c6520116
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-09-22 01:13:56 +0300

    Recaps coz how did i end up here

---
+++ Crates/Http_Reqwest/http_test_project/target/debug/deps/test_http-e79561a2fe88d60d.d
@@ -0,0 +1,6 @@
+D:\playground\Rustplayground\Crates\test_http\target\debug\deps\test_http-e79561a2fe88d60d.d: src\main.rs src\../index.html
+
+D:\playground\Rustplayground\Crates\test_http\target\debug\deps\libtest_http-e79561a2fe88d60d.rmeta: src\main.rs src\../index.html
+
+src\main.rs:
+src\../index.html:
