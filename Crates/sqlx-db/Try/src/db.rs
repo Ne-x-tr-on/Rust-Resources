4b5db6002725b62779ac0455f6288c9116105d64
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-10-20 02:41:55 +0300

    _" git push "

--- Crates/sqlx-db/Try/src/db.rs
+++ Crates/sqlx-db/Try/src/db.rs
@@ -1,4 +1,26 @@
-use sqlx::{postgres::PgOptionPool,PgPool};
-use dotenvy;
+use sqlx::{postgres::PgPoolOptions,PgPool};
+// use serde::{Serialize,Deserialize};
+use dotenvy::dotenv;
 use std::env;
-use serde;
+
+pub async fn create_connection() -> Result<PgPool,sqlx::Error>{
+  dotenv().ok();
+
+  let db_url = env::var("DATABASE_URL").expect("Failed to read the database");
+
+  // Connection Pool
+  let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&db_url)
+    .await
+    .expect("Failed to connect to the database");
+
+  sqlx::migrate!("./migrations")
+    .run(&pool)
+    .await
+    .expect("Failed to run db migrations");
+
+
+  Ok(pool)
+
+}
--- Crates/sqlx-db/Try/src/main.rs
+++ Crates/sqlx-db/Try/src/main.rs
@@ -9,6 +9,43 @@ use axum::{
     response::IntoResponse,
     http::StatusCode,
 };
-fn main() {
-    println!("Hello, world!");
+
+use db::create_connection;
+
+#[tokio::main]
+async fn main() {
+    let pool = create_connection().await;
+
+    let app = Router::new().route("/", get(|| async { "✅ Database connected!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Server running on http://127.0.0.1:3000");
+
+    axum::serve(listener, app).await.unwrap();
 }
+
+
+// use axum::{Router, routing::get, extract::State};
+// use std::sync::Arc;
+
+// #[tokio::main]
+// async fn main() -> Result<(), sqlx::Error> {
+// Atomic Reference Counted
+//     let pool = Arc::new(create_connection().await?);
+
+//     let app = Router::new()
+//         .route("/health", get(health_check))
+//         .with_state(pool.clone());
+
+//     println!("✅ Server running...");
+//     Ok(())
+// }
+
+// async fn health_check(State(pool): State<Arc<PgPool>>) -> String {
+//     if sqlx::query!("SELECT 1").fetch_one(&*pool).await.is_ok() {
+//         "Database OK ✅".to_string()
+//     } else {
+//         "Database Error ❌".to_string()
+//     }
+// }
+
